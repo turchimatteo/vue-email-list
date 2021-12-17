445a303d3bb30d7af8560d892963842cef7ff3ed
turchimatteo <turchi12@gmail.com>
2021-12-17 17:14:54 +0100

    email stampate in console

---
+++ js/script.js
@@ -0,0 +1,26 @@
+/* Descrizione:
+Attraverso l'apposita API di Boolean
+https://flynn.boolean.careers/exercises/api/random/mail
+generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+Bonus
+Far comparire gli indirizzi email solamente quando sono stati tutti generati. */
+
+Vue.config.devtools = true;
+const app = new Vue(
+    {
+        el: '#root',
+        data: {
+            emails: []
+        },
+        created: function() {
+            for (let i = 0; i < 10; i++) {
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then((response) => {
+                    const newEmail = response.data.response;
+                    this.emails.push(newEmail);
+                    console.log(newEmail);
+                });
+            }
+        }
+    }
+);
